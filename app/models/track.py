30388680eb9c10b4c609ca9a273ba69c09e61dbd
daniel frias <danifriassolsona@gmail.com>
2022-02-18 18:55:18 +0100

    soy jesus2

--- app/models/track.py
+++ app/models/track.py
@@ -5,7 +5,7 @@ class Track(db.Model, BaseModelMixin):
     __tablename__ = "Track"
     id = db.Column(name="ArtistId", type_=db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(name="Name", type_=db.Unicode(120), nullable=False, unique=True)
-    media_type_id = db.column(name="MediaTypeId", type_=db.Integer, nullable=False, default=1)
+    media_type_id = db.Column(name="MediaTypeId", type_=db.Integer, nullable=False, default=1)
     composer = db.Column(name="Composer", type_=db.Unicode(220))
     milliseconds = db.Column(name="Milliseconds", type_=db.Integer, nullable=False)
     bytes = db.Column(name="Bytes", type_=db.Integer, default=None, nullable=False)
